02681defbe5a5db73f52f76f2d2ec29dc94c1a9b
Karim Farid <karim.f.1995@gmail.com>
2015-12-15 19:18:55 +0200

    updated relations

--- Server/db/migrate/20151215154911_create_events.rb
+++ Server/db/migrate/20151215154911_create_events.rb
@@ -5,7 +5,8 @@ class CreateEvents < ActiveRecord::Migration
       t.string :description
       t.string :place
       t.timestamp :time
-
+      t.integer :creator_id
+      t.foreign_key :creator_id
       t.timestamps null: false
     end
   end
--- Server/db/migrate/20151215155304_create_tasks.rb
+++ Server/db/migrate/20151215155304_create_tasks.rb
@@ -4,7 +4,10 @@ class CreateTasks < ActiveRecord::Migration
       t.string :title
       t.string :description
       t.string :status
-
+      t.integer :assigner_id
+      t.integer :assignee_id
+      t.foreign_key :assigner_id
+      t.foreign_key :assignee_id
       t.timestamps null: false
     end
   end
