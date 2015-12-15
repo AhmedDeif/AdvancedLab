7aa1ef992b14c7eebfae39ade06572cfee940558
Karim Farid <karim.f.1995@gmail.com>
2015-12-15 19:07:35 +0200

    latest

--- Server/db/migrate/20151215155203_create_users.rb
+++ Server/db/migrate/20151215155203_create_users.rb
@@ -6,7 +6,12 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :oauth_token
       t.datetime :oauth_expires_at
-
+      t.string :email
+      t.time :dateOfBirth
+      t.string :country
+      t.string :committee
+      t.string :city
+      t.string :gender
       t.timestamps null: false
     end
   end
